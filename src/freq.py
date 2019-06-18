4e53426e5ccd6fed6d1653e1442afb9ed819b6c4
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-17 22:29:28 -0300

    pandas to_dataframe now as fast as hell to convert; datetime.datetime removed

--- src/freq.py
+++ src/freq.py
@@ -8,8 +8,6 @@
 import sys
 import os
 
-from datetime import datetime, timedelta
-
 import pandas as pd
 import numpy as np
 pd.set_option('display.max_colwidth', -1)
@@ -29,11 +27,10 @@ class EarthQuakeData:
         self.data = pd.read_csv('../MC3/data/YInt.csv')
 
         print('converting from strtime to datetime...')
-        for i in range(len(self.data)):
-            time = self.data['time'][i]
-            self.data['time'][i] = self.str2datetime(time)
+        self.data['time'] = pd.to_datetime(self.data['time'], format='%Y-%m-%d %H:%M:%S')
 
     def sortby(self, col):
+        print('ordenando')
         self.data.sort_values(by=[col])
 
     def splitby_time(self):
@@ -45,14 +42,13 @@ class EarthQuakeData:
         time_chunks = []
         count = 0
         while curr_date < last_date:
-            next_date = curr_date + timedelta(minutes=TIME_INTERVAL)
-            #print(first_date, '|||', curr_date, '@@', next_date, '|||', last_date)
+            next_date = curr_date + np.timedelta64(TIME_INTERVAL, 'm')
             sys.stdout.flush()
 
             if next_date > last_date:
                 next_date = last_date
             while not self.data.isin({'time':[next_date]})['time'].any():
-                next_date += timedelta(minutes=1)
+                next_date += np.timedelta64(1, 'm')
 
             data = self.data[self.data['time'].between(curr_date, next_date)]
             tc = TimeChunks(data)
@@ -62,28 +58,24 @@ class EarthQuakeData:
         return time_chunks
 
     def str2datetime(self, strdate):
-        return datetime.strptime(strdate, '%Y-%m-%d %H:%M:%S')
+        return None # FIXME
 
     def datetime2str(self, dtdate):
-        return dtdate.strftime('%Y-%m-%d %H:%M:%S')
+        return None # FIXME
 
 class TimeChunks:
     def __init__(self, data):
         super(TimeChunks, self).__init__()
         self.data = data
 
-    def str2datetime(self, strdate):
-        return datetime.strptime(strdate, '%Y-%m-%d %H:%M:%S')
-
-    def datetime2str(self, dtdate):
-        return dtdate.strftime('%Y-%m-%d %H:%M:%S')
-
-def plot_bar_tweet_freq(vec):
+def plot_bar_tweet_freq(data):
     print('plotting bar chart...')
+    vec = [len(ch.data) for ch in data]
     size = len(vec)
     xticklabels = []
     for i in range(0, size+1, 10):
-        xticklabels.append('%s' % chunks[i].data['time'].head(1).get_values()[0].strftime('%m/%d\n%H:%M'))
+        xticklabels.append('%s' % pd.to_datetime(data[i].data['time']
+                    .head(1).get_values()[0]).strftime('%b/%d\n%H:%M')) # 
 
     plt.ylabel('Frequency', fontsize=32)
     plt.xlabel('Time intervals', fontsize=32)
@@ -99,12 +91,6 @@ def plot_bar_tweet_freq(vec):
 if __name__=='__main__':
     eq = EarthQuakeData()
     eq.sortby('time')
-
-    chunks = eq.splitby_time()
-
-    lengths = []
-    for i, ch in enumerate(chunks):
-        lengths.append(len(ch.data))
-
-    plot_bar_tweet_freq(lengths)
+    #time_intervals = eq.splitby_time()
+    #plot_bar_tweet_freq(time_intervals)
 ### EOF ###
