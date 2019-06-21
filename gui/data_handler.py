951ecc0f05b7c815d4168f6e87285ecdd88def9a
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-21 20:18:58 -0300

    first version of grid layout with bar charts reasonably working

--- gui/data_handler.py
+++ gui/data_handler.py
@@ -3,11 +3,18 @@
 # author: june 2019
 # cassio batista - cassio.batista.13@gmail.com
 
+import sys
 import pandas as pd
 import numpy as np
+pd.set_option('display.max_rows', None)  
+pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_columns', None) 
 
 DATA_CSVFILE = '../MC3/data/YInt.csv'
 REPLACE_FILE = '../src/replace.txt'
+USELESS_FILE = '../src/w_useless.txt'
+USEFUL_FILE  = '../src/w_useful.txt'
+TAG = 'DH'
 
 def get_replace_rules():
     rules = {}
@@ -17,12 +24,34 @@ def get_replace_rules():
             rules[key] = value
     return rules
 
-def load_data():
-    data = pd.read_csv(DATA_CSVFILE, parse_dates=['time'])
-    mapping_rules = get_replace_rules()
-    for i in range(len(data['message'])):
-        
-    #for wrong, correct in mapping_rules.items():
-        
+def get_useless_words():
+    wordlist = []
+    with open(USELESS_FILE) as f:
+        for line in f:
+            word, freq = line.split(':')
+            wordlist.append(word)
+    return wordlist
 
-load_data()
+def get_useful_words():
+    wordlist = []
+    with open(USEFUL_FILE) as f:
+        for line in f:
+            word, freq = line.split(':')
+            wordlist.append(word)
+    return wordlist
+
+def preprocess(data):
+    mapping_rules = get_replace_rules()
+    print(TAG, 'replacing wrong words')
+    for wrong, correct in mapping_rules.items():
+        data['message'] = data['message'].str.replace(wrong, correct)
+    print(TAG, 'lowercasing text cols')
+    for col in data.columns:
+        if col != 'time':
+            data[col] = data[col].str.lower()
+    return data
+
+def load_data():
+    print(TAG, 'loading data')
+    data = pd.read_csv(DATA_CSVFILE, parse_dates=['time'])
+    return preprocess(data)
---
+++ gui/words_per_loc_bar.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+
+from datetime import date
+
+import collections
+import math
+
+import pandas as pd
+import numpy as np
+
+from bokeh.document import Document
+from bokeh.embed import file_html
+from bokeh.resources import INLINE
+from bokeh.util.browser import view
+from bokeh.models.layouts import Row, Column
+from bokeh.models.widgets import DateRangeSlider
+
+from bokeh.io import output_file, show
+from bokeh.layouts import gridplot
+from bokeh.palettes import Viridis3
+from bokeh.plotting import figure
+
+from bokeh.models.annotations import Title
+from bokeh.models import ColumnDataSource, Plot, LinearAxis, Grid, LabelSet, Label
+from bokeh.models.glyphs import HBar
+
+from data_handler import load_data, get_useless_words, get_useful_words
+
+data = load_data()
+w_useless = get_useless_words()
+w_useful  = get_useful_words()
+
+word_count = {}
+for location in data.location.unique():
+    word_count[location] = {}
+
+for location, tweet in zip(data['location'], data['message']): # TODO add time filter
+    if isinstance(tweet, str):
+        tweet = re.sub('[#@!?,.;]', ' ', tweet)
+        for word in tweet.split():
+            if len(word) > 3:
+                word = word[:4]
+                if word in w_useless:
+                    continue
+                if word in word_count[location]:
+                    word_count[location][word] += 1
+                else:
+                    word_count[location][word]  = 1
+plots = []
+for key, value in word_count.items():
+    y = np.arange(10)
+    wordfreqlist = sorted(value.items(), key=lambda kv: kv[1], reverse=True)
+    x = []
+    words = []
+    for i, element in enumerate(wordfreqlist):
+        word, freq = element
+        x.append(freq)
+        words.append(word)
+        if i == 9:
+            break
+    source = ColumnDataSource(dict(y=y, right=x,))
+    plt = Plot( 
+                title=None, plot_width=60, plot_height=400,
+                min_border=0, toolbar_location=None)
+    t = Title()
+    t.text = key.replace(' ', '\n')
+    plt.title = t
+    glyph = HBar(y='y', right='right', left=0, height=0.85, fill_color="#b3de69")
+    plt.add_glyph(source, glyph)
+
+    xaxis = LinearAxis()
+    plt.add_layout(xaxis, 'below')
+    plt.xaxis.major_label_orientation = math.pi/2
+    
+    #plt.add_layout(yaxis, 'left')
+
+    plt.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+    #plt.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+    #labels = LabelSet(
+    #            x='x', y='y', text='y', level='glyph',
+    #            #x_offset=5, y_offset=5, 
+    #            source=source, render_mode='canvas')
+    #plt.add_layout(labels)
+    for i, word in enumerate(words):
+        note = Label(x=5, y=i*34, x_units='screen', y_units='screen',
+                    text=word, render_mode='css',
+                    border_line_color='black',
+                    border_line_alpha=1.0,
+                    background_fill_color='white',
+                    background_fill_alpha=0.5)
+        plt.add_layout(note)
+    plots.append(plt)
+
+# make a grid
+grid = gridplot([
+    plots,
+])
+
+print(data['time'].iloc[0])
+date_range_slider = DateRangeSlider(
+            start  = data['time'].iloc[0],
+            end    = data['time'].iloc[-1],
+            value  = (data['time'].iloc[1000], data['time'].iloc[-1000]),
+            step   = 1)
+
+sliders = Row(children=[
+    Column(children=[
+        grid,
+        date_range_slider,
+    ]),
+])
+
+doc = Document()
+doc.add_root(sliders)
+
+if __name__ == "__main__":
+    doc.validate()
+    filename = "sliders.html"
+    with open(filename, "w") as f:
+        f.write(file_html(doc, INLINE, "sliders"))
+    view(filename)
