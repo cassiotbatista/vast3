a5cd26b653b27400c29794018c2bd242a8e2ed7c
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-27 14:11:47 -0300

    fix #6 and #11: hover over bar to show whole word lists

--- gui/words_per_loc_bar.py
+++ gui/words_per_loc_bar.py
@@ -20,7 +20,7 @@ from bokeh.io import curdoc
 from bokeh.models.layouts import Row, Column
 from bokeh.models.widgets import DateRangeSlider, Button
 from bokeh.models.annotations import Title
-from bokeh.models import ColumnDataSource, Plot, LinearAxis, Grid, LabelSet, Label, ColorBar, FixedTicker
+from bokeh.models import ColumnDataSource, Plot, LinearAxis, Grid, LabelSet, Label, ColorBar, FixedTicker, HoverTool
 from bokeh.models.glyphs import HBar, VBar
 from bokeh.layouts import gridplot
 
@@ -42,9 +42,10 @@ color_bar = ColorBar(
         width        = 8,
         location     = (0,0))
 
-word_count    = OrderedDict()
-user_count    = OrderedDict()
-mention_count = OrderedDict()
+prefix_count     = OrderedDict()
+wword_count      = OrderedDict()
+user_count       = OrderedDict()
+mention_count    = OrderedDict()
 
 word_barplots   = []
 word_sources    = []
@@ -69,13 +70,16 @@ def init_word_barplots():
     global word_sources
     word_barplots = []
     word_sources  = []
-    for i in range(len(word_count)):
-        src = ColumnDataSource(dict(y=[], right=[],))
+    for i in range(len(prefix_count)):
+        src = ColumnDataSource(dict(y=[], right=[], wlist=[],))
         plt = Plot(
             title            = None, 
             plot_width       = 95, 
             plot_height      = 400,
-            min_border       = 0, 
+            min_border       = 0,
+            tools = [HoverTool(tooltips=[
+                    ('wlist', '@wlist'),
+                ])],
             toolbar_location = None)
         word_barplots.append(plt)
         word_sources.append(src)
@@ -89,17 +93,17 @@ def init_word_barplots():
     word_barplots.append(plt)
 
 def init_wordcount():
-    global word_count
-    word_count = OrderedDict()
+    global prefix_count
+    prefix_count = OrderedDict()
     for location in data.location.unique():
         if location.startswith('unk') or location.startswith('<loc'):
             continue
-        word_count[location] = {}
+        prefix_count[location] = {}
 
 init_wordcount()
 init_word_barplots()
 
-def count_words(word_count):
+def count_words(prefix_count, wword_count):
     date_value = date_range_slider.value_as_datetime
     data_chunk = data[data.time.between(date_value[0], date_value[1])]
     for location, tweet in zip(data_chunk.location, data_chunk.message): 
@@ -107,16 +111,25 @@ def count_words(word_count):
             continue
         if not isinstance(tweet, str):
             continue
-        tweet = re.sub('[#@!?,.;]', ' ', tweet)
+        tweet = re.sub('[#!?,.;\-$"\*/\')(><]', ' ', tweet)
         for word in tweet.split():
+            if word.startswith('@') or word.count(':') > 1:
+                continue
+            word = re.sub(r'([iauhy])\1+', r'\1', word)
             if len(word) > MIN_WLEN:
-                word = word[:MIN_WLEN+1]
-                if word in useless_wordlist:
+                prefix = word[:MIN_WLEN+1]
+                if prefix in useless_wordlist:
                     continue
-                if word in word_count[location]:
-                    word_count[location][word] += 1
+                if prefix in prefix_count[location]:
+                    prefix_count[location][prefix] += 1
                 else:
-                    word_count[location][word]  = 1
+                    prefix_count[location][prefix]  = 1
+                if not prefix in wword_count:
+                    wword_count[prefix] = {}
+                if word in wword_count[prefix]:
+                    wword_count[prefix][word] += 1
+                else:
+                    wword_count[prefix][word]  = 1
 
 def count_users():
     user_count = data.account.value_counts()
@@ -135,9 +148,9 @@ def count_mentions():
                         mention_count[word] = 1
     return sorted(mention_count.items(), key=lambda kv: kv[1], reverse=True)[:30]
 
-def get_freq_range(word_count):
+def get_freq_range(prefix_count):
     frequencies = []
-    for wordcountdict in word_count.values():
+    for wordcountdict in prefix_count.values():
         wordfreqlist = sorted(wordcountdict.items(), 
                     key=lambda kv: kv[1], reverse=True)
         for word, freq in wordfreqlist[:NUM_WORDS]:
@@ -205,7 +218,8 @@ def init_mention_plot():
 
 def init_plot():
     init_wordcount()
-    global word_count
+    global prefix_count
+    global wword_count
     global word_barplots
     global word_sources
     global mapper
@@ -213,10 +227,10 @@ def init_plot():
     init_user_plot()
     init_mention_plot()
 
-    count_words(word_count)
+    count_words(prefix_count, wword_count)
 
     # update colorbar
-    min_freq, max_freq = get_freq_range(word_count)
+    min_freq, max_freq = get_freq_range(prefix_count)
     mapper['transform'].low  = min_freq
     mapper['transform'].high = max_freq
 
@@ -228,17 +242,20 @@ def init_plot():
                     max_freq, steps, dtype=np.int))
 
     y = np.arange(NUM_WORDS)
-    for i, (neigh,wcount) in enumerate(word_count.items()):
+    for i, (neigh,wcount) in enumerate(prefix_count.items()):
         wordfreqlist = sorted(wcount.items(), key=lambda kv: kv[1], reverse=True)
         x = []
-        words = []
-        for word, freq in wordfreqlist[:NUM_WORDS]:
+        prefixes = []
+        wlist = []
+        for prefix, freq in wordfreqlist[:NUM_WORDS]:
             x.append(freq)
-            words.append(word)
+            prefixes.append(prefix)
+            wlist.append(sorted(wword_count[prefix].items(), key=lambda
+                kv:kv[1], reverse=True)[:5])
 
         plt = word_barplots[i]
         src = word_sources[i]
-        src.data = dict(y=y, right=x)
+        src.data = dict(y=y, right=x, wlist=wlist)
 
         t = Title()
         t.text = neigh[:13]
@@ -255,7 +272,7 @@ def init_plot():
         
         yaxis = LinearAxis()
         yaxis.ticker = y
-        yaxis.major_label_overrides = { i : word for i, word in enumerate(words) }
+        yaxis.major_label_overrides = { i : prefix for i, prefix in enumerate(prefixes) }
         yaxis.major_label_standoff = -35
         plt.add_layout(yaxis, 'left')
 
@@ -264,16 +281,17 @@ def init_plot():
 
 def update():
     init_wordcount()
-    global word_count
+    global prefix_count
+    global wword_count
     global word_barplots
     global word_sources
     global mapper
     global color_bar
 
-    count_words(word_count)
+    count_words(prefix_count, wword_count)
 
     # update mapper
-    min_freq, max_freq = get_freq_range(word_count)
+    min_freq, max_freq = get_freq_range(prefix_count)
     mapper['transform'].low  = min_freq
     mapper['transform'].high = max_freq
 
@@ -285,7 +303,7 @@ def update():
                     max_freq, steps, dtype=np.int))
 
     y = np.arange(NUM_WORDS)
-    for i, (neigh,wcount) in enumerate(word_count.items()):
+    for i, (neigh,wcount) in enumerate(prefix_count.items()):
         wordfreqlist = sorted(wcount.items(), key=lambda kv: kv[1], reverse=True)
         x = []
         words = []
