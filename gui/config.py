12871b23986b3971a17ed5c0991a4306b040ab5e
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-29 17:49:19 -0300

    svgfile udpated on config to the one with labels

--- gui/config.py
+++ gui/config.py
@@ -4,13 +4,13 @@ TAG = 'cfg'
 
 DO_LEMMATIZE = False
 if not DO_LEMMATIZE:
-    cprint('%s: lemmatize will be skipped to speed up processing' % TAG, 
+    cprint('%s: lemmatize will be skipped to speed up the process' % TAG, 
             'red', attrs=['bold'])
 
 NUM_WORDS = 10
 MIN_WLEN  = 3
 
-SVGFILE = '../MC3/maps/cassomap.svg'
+SVGFILE = '../MC3/maps/custom/neigh_labels.svg'
 
 DATA_CSVFILE = '../MC3/data/YInt.csv'
 REPLACE_FILE = '../src/replace.txt'
