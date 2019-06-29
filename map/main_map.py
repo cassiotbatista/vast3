a9b615d3711e1c99218a1698addac0e01b493a9b
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-29 10:38:50 -0300

    useless fig attribs removed

--- map/main_map.py
+++ map/main_map.py
@@ -1,11 +1,9 @@
 from bokeh.layouts import layout, row
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource, CustomJS
-from bokeh.models.widgets import Div, Button, RadioButtonGroup, Select, Slider
-from bokeh.events import ButtonClick
+from bokeh.models.widgets import Div, Button
 from bokeh.plotting import figure
 from bokeh.palettes import Spectral6
-from bokeh.models import Range1d, PanTool, ResetTool, HoverTool, WheelZoomTool
+from bokeh.models import ResetTool, HoverTool
 
 import re
 from termcolor import cprint
@@ -14,9 +12,9 @@ from svg import SVG
 
 svg = SVG()
 def file_callback():
-    svg.svg_struct['oldtown']['style'] = re.sub('fill:#(.*?);', 
-                    'fill:#ff0000;', svg.svg_struct['oldtown']['style'])
-    notification = Div(text=svg.join_attrs(svg.svg_struct), width=550, height=500)
+    svg.change_fill_color('oldtown', 'ff0000')
+    svg.update_svg_text()
+    notification = Div(text=svg.get_text(), width=550, height=500)
     layout = curdoc().get_model_by_name('mainLayout')
     layout.children.pop()
     layout.children.append(notification)
@@ -25,19 +23,12 @@ button_2 = Button(label="vai caralho")
 button_2.on_click(file_callback)
 
 # put the results in a row
-notification = Div(text=svg.raw_text, width=550, height=500)
+notification = Div(text=svg.raw_text, width=50, height=50)
 
 f = figure(x_range = [], plot_height=250, title="Some Plot", toolbar_location=None, tools="", name = 'plot')
-f.vbar([], top=[], width=0.9, color=Spectral6)
-f.y_range.start = 0
-f.tools = [PanTool(),ResetTool()]
-f.toolbar_location = 'above'
-f.plot_width=800
-f.plot_height=500
 
 lay = row(button_2, f, width=800,name='mainLayout')
 lay.children.pop()
 lay.children.append(notification)
 
 curdoc().add_root(lay)
-curdoc().title = "Word Count"
