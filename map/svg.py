60dee3ccb6602081d66312aab3783a3522daed8f
Cassio Batista <cassio.batista.13@gmail.com>
2019-06-29 00:49:39 -0300

    svg: joint_attrs method implemented; methods now receive params for futher access from another class

--- map/svg.py
+++ map/svg.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import re
 from svgpathtools import svg2paths
 
 class SVG:
@@ -5,31 +8,43 @@ class SVG:
     def __init__(self):
         super(SVG, self).__init__()
         self.raw_text = self.load()
+        self.svg_text = self.raw_text
         self.paths, self.attrs, self.metadata = svg2paths(
                         self.SVGFILE, return_svg_attributes=True)
-        self.svg = self.split_attrs()
+        self.svg_struct = self.split_attrs(self.attrs)
+        print(self.metadata)
 
     def load(self):
         with open(self.SVGFILE, 'r') as f:
             svg = f.read()
         return svg
 
-    def split_attrs(self):
+    def split_attrs(self, attributes):
+        if not isinstance(attributes, list):
+            print('there\'s something really wrong around here')
         svg = {}
-        for pathtag in self.attrs:
-            identifier = pathtag['id']
+        for path_tag in attributes:
+            identifier = path_tag['id']
             svg[identifier] = {}
-            for key, value in pathtag.items():
+            for key, value in path_tag.items():
                 if key != 'id':
                     svg[identifier][key] = value
         return svg
 
-    def join_attrs(self):
-        svg = {}
-        for pathtag in self.attrs:
-            identifier = pathtag['id']
-            svg[identifier] = {}
-            for key, value in pathtag.items():
-                if key != 'id':
-                    svg[identifier][key] = value
+    def join_attrs(self, svg_structure):
+        if not isinstance(svg_structure, dict):
+            print('there\'s something really wrong around here')
+        svg = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + '\n'
+        svg += '<svg' + '\n'
+        for key, value in self.metadata:
+            svg += '    %s="%s"' % (key, value) + '\n'
+        svg += '>' # TODO rstrip()
+        svg += '  <g>' # FIXME I'm losing metadata here
+        for identifier in svg_structure:
+            svg += '    <path id="%s"' % identifier + '\n'
+                for key, value in svg_structure[identifier].items():
+                    svg += '        %s="%s"' % (key, value) + '\n'
+            svg += '    />' # TODO rstrip()
+        svg += '  </g>'
+        svg += '</svg>'
         return svg
