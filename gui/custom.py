e884f7ff7206a92ed872e94d36f6c8072c8e3e2d
Cassio Batista <cassio.batista.13@gmail.com>
2019-07-04 15:24:02 -0300

    horizon examples maintained on new branch thjerefore rm from master

--- gui/custom.py
+++
@@ -1,8 +0,0 @@
-from bokeh.core.properties import Instance, Override
-from bokeh.models import HTMLBox, Slider
-
-class Custom(HTMLBox):
-    __implementation__ = 'custom.ts'
-
-    slider = Instance(Slider)
-    margin = Override(default=5)
