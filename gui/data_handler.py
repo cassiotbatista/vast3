829c16ff895b45081ce687c90dac21a1b09bf728
Cassio Batista <cassio.batista.13@gmail.com>
2019-07-06 13:23:27 -0300

    data_handler refact preprocess

--- gui/data_handler.py
+++ gui/data_handler.py
@@ -30,7 +30,8 @@ TAG = 'DH'
 tokenizer  = WhitespaceTokenizer()
 lemmatizer = WordNetLemmatizer()
 
-blob_count = 0
+blob_count  = 0
+lemma_count = 0
 
 def get_replace_rules():
     rules = {}
@@ -60,7 +61,7 @@ def get_user_blacklist():
     blacklist = []
     with open(config.BLACKLIST_FILE) as f:
         for user in f:
-            blacklist.append(user.strip('@'))
+            blacklist.append(user.strip('@').rstrip())
     return blacklist
 
 def get_keywords():
@@ -71,26 +72,33 @@ def get_keywords():
             wordlist.append(prefix)
     return wordlist
 
-def lemmatize(text, pos):
-    tokens = [lemmatizer.lemmatize(word, pos=pos) \
-            for word in tokenizer.tokenize(text)]
-    return ' '.join(tokens)
-
 def check_spell(text):
     global blob_count
-    sys.stdout.write('\r%06d' % blob_count)
-    sys.stdout.flush()
     blob_count += 1
     blob = TextBlob(text).correct()
+    sys.stdout.write('\r\t\t\t\t\t\t\t\t%05d' % blob_count)
+    sys.stdout.flush()
     return blob.raw
 
-def reduce_lengthening(data):
-    data['message'] = data.message.str.replace(r'(.)\1{2,}',r'\1\1')
-    return data
+def lemmatize(text):
+    global lemma_count
+    lemma_count += 1
+    tokens = [lemmatizer.lemmatize(lemmatizer.lemmatize(word, 'n'), 'v') \
+            for word in tokenizer.tokenize(text)]
+    sys.stdout.write('\r\t\t\t\t\t\t\t\t%05d' % lemma_count)
+    sys.stdout.flush()
+    return ' '.join(tokens)
 
 def normalise(data):
-    data['message'] = data.message.str.replace(r'[#!?,.;\-$"\*/)(><\']', ' ')
-    data['message'] = data.message.str.replace(r'([qwyuiahjkxv])\1+', r'\1')
+    data.message = data.message.str.replace(r'[=!?,.;\-$"\*/)(><\']', ' ')
+    data.message = data.message.str.replace(r'([qwyuiahjkxv])\1+', r'\1')
+    data.message = data.message.str.replace(r'(.)\1{2,}',r'\1\1')
+    return data
+
+def stringify(data):
+    for col in data.columns:
+        if col != 'time':
+            data[col] = data[col].apply(str)
     return data
 
 def lowercase(data):
@@ -100,34 +108,43 @@ def lowercase(data):
     return data
 
 def preprocess(data):
-    cprint('%s: converting message column to str' % TAG, 'green', attrs=['bold'])
-    data['message'] = data.message.apply(str)
-    cprint('%s: lowercasing text cols' % TAG, 'green', attrs=['bold'])
+    cprint('%s: converting message column to str' % TAG, 
+            'green', attrs=['bold'])
+    data = stringify(data)
+    cprint('%s: lowercasing text cols' % TAG, 
+            'green', attrs=['bold'])
     data = lowercase(data)
-    if config.DO_BLACK_SELECT:
-        cprint('%s: selecting tweets by username' % TAG, 'green', attrs=['bold'])
-        blackgex = '|'.join(get_user_blacklist())
-        data = data[~data['account'].str.contains(blackgex)]
-    cprint('%s: replacing wrong words' % TAG, 'green', attrs=['bold'])
+    cprint('%s: replacing wrong words' % TAG, 
+            'green', attrs=['bold'])
     for wrong, correct in get_replace_rules():
-        data['message'] = data['message'].str.replace(wrong, correct)
-    cprint('%s: normalising chars' % TAG, 'green', attrs=['bold'])
+        data.message = data.message.str.replace(wrong, correct)
+    cprint('%s: normalising chars' % TAG, 
+            'green', attrs=['bold'])
     data = normalise(data)
-    cprint('%s: removing chars redundancy' % TAG, 'green', attrs=['bold'])
-    data = reduce_lengthening(data)
+    if config.DO_BLACK_SELECT:
+        cprint('%s: selecting tweets by username' % TAG, 
+                'green', attrs=['bold'])
+        blackgex = '|'.join(get_user_blacklist())
+        data = data.loc[data.account.str.contains(blackgex) == False]
+    if config.DO_KW_SELECT:
+        cprint('%s: selecting tweets by keyword' % TAG, 
+                'green', attrs=['bold'])
+        wordgex = '|'.join(get_keywords())
+        data = data.loc[data.message.str.contains(wordgex)]
     if config.DO_SPELLCHECK:
         cprint('%s: checking spell (it may take a while...) ' % TAG, 
-                'green', attrs=['bold', 'blink'])
-        data['message'] = data.message.apply(check_spell)
+                'green', attrs=['bold', 'blink'], end=' ')
+        start = time.time()
+        data.message = data.message.apply(check_spell)
+        end = time.time()
+        print('\t(%.2f seconds)' % (end - start))
     if config.DO_LEMMATIZE:
-        cprint('%s: lemmatizing words twice (it may take a while...)' % TAG, 
-                'green', attrs=['bold', 'blink'])
-        data['message'] = data.message.apply(lemmatize, args=('n'))
-        data['message'] = data.message.apply(lemmatize, args=('v'))
-    if config.DO_KW_SELECT:
-        cprint('%s: selecting tweets by keyword' % TAG, 'green', attrs=['bold'])
-        wordgex = '|'.join(get_keywords())
-        data = data[data['message'].str.contains(wordgex)]
+        cprint('%s: lemmatizing verbs and nouns (it may take a while...)' % TAG, 
+                'green', attrs=['bold', 'blink'], end=' ')
+        start = time.time()
+        data.message = data.message.apply(lemmatize)
+        end = time.time()
+        print('\t(%.2f seconds)' % (end - start))
     return data
 
 def load_data():
@@ -141,3 +158,5 @@ if __name__=='__main__':
     data = preprocess(pd.read_csv(config.DATA_SRC_CSVFILE, parse_dates=['time']))
     with open(config.DATA_PROC_CSVFILE, 'w') as f:
         data.to_csv(f, index=False, quoting=1)
+    cprint('%s: data written to "%s"' % (TAG, config.DATA_PROC_CSVFILE), 
+            'green', attrs=['bold'])
